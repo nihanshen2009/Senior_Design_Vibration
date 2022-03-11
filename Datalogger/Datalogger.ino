9246f5c53fa38436cd7f87a4dbbf9306da300736
opceryu <yigitkaratas1@gmail.com>
2022-03-11 14:51:24 -0500

    Commented the the Arduino code

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -17,28 +17,34 @@ volatile bool activeBuff = 0;
 volatile bool full = 0;
 const int fileNum;
 
-
 void setup()
 {
-  delay(500);
-  pinMode(17, OUTPUT);
-  Serial.begin(9600);
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+  delay(500); //initial delay to make sure everything powers up
+  pinMode(17, OUTPUT); //debug pin for interrupt
+  Serial.begin(9600); //serial for debugging
+  
+  //SD card initilization
+  if (!SD.begin(chipSelect)) { 
+    Serial.println("Card failed, or not present"); 
     while (1) {
       // No SD card, so don't do anything more - stay stuck here
     }
   }
   else
     Serial.println("card initialized.");
+
+  //I2C line initilization and setting clock speed
   Wire.begin();
   Wire.setClock(1000000);
+
+  //Accel initilization
   if( !kxAccel.begin() ){
-    Serial.println("Could not communicate with the the KX13X. Freezing.");
+    Serial.println("Could not communicate with the the KX14X. Freezing.");
     while(1);
   }
   else
     Serial.println("KX13X Ready.");
+  //Accel Default setting loading
   if( !kxAccel.initialize(DEFAULT_SETTINGS)){ // Loading default settings.
     Serial.println("Could not initialize the KX13X chip.");
     while(1);
@@ -46,56 +52,57 @@ void setup()
   else {
     Serial.println("All systems initialized...");
     Serial.println("Convertion factor: ");
-    Serial.print(.0002441407513657033);
+    Serial.print(.0002441407513657033); //Convertion multiplier for raw data (in g)
   }
-  Serial.println(kxAccel.setOutputDataRate(0b1011));
-  
+  //Accel output data rate
+  Serial.println(kxAccel.setOutputDataRate(0b1011)); //test the change to 0b1100
+
+  //Timer interrupt initilization
   myTimer.begin(measure_time, 625);
 }
 
 void loop()
 {
-  
+  //if one of the buffers are full
   if (full){
-    File dataFile = SD.open("datalog.txt", FILE_WRITE);
-    if (dataFile) {
-      if (activeBuff == 0) {
+    File dataFile = SD.open("datalog.txt", FILE_WRITE); //Begin writting to the data file
+    if (dataFile) { //if data file is intilized
+      if (activeBuff == 0) { //Choose the active buffer and write the data to SD card
         dataFile.write(buff_1, sizeof(buff_1));
       } else {
         dataFile.write(buff_2, sizeof(buff_2));
       }
-      dataFile.close();
-      full = 0;
-      Serial.println("written");
+      dataFile.close(); //close the data file
+      full = 0; //set the full flag to 0
+      Serial.println("written"); //Serial print for debugging
     } else {
-      Serial.println("file error");
+      Serial.println("file error"); //if file is unavailable error
     }
   }
   
 }
  
 void measure_time () {
-  digitalWrite(17, 1);
+  digitalWrite(17, 1); //set the debugging pin high
   
-  if(activeBuff == 0) {
-    buff_1[ind] = micros();
-    buff_1[ind+1] = rawData.xData;
-    buff_1[ind+2] = rawData.yData;
-    buff_1[ind+3] = rawData.zData;
+  if(activeBuff == 0) { //write the data to appropriate buffer
+    buff_1[ind] = micros(); //time
+    buff_1[ind+1] = rawData.xData; //x accel
+    buff_1[ind+2] = rawData.yData; //y accel
+    buff_1[ind+3] = rawData.zData; //z accel
   } else {
-    buff_2[ind] = micros();
-    buff_2[ind+1] = rawData.xData;
-    buff_2[ind+2] = rawData.yData;
-    buff_2[ind+3] = rawData.zData;
+    buff_2[ind] = micros(); //time
+    buff_2[ind+1] = rawData.xData; //x accel
+    buff_2[ind+2] = rawData.yData; //y accel
+    buff_2[ind+3] = rawData.zData; //z accel
   }
-  ind += 4;
-  if (ind >= 512) {
-    ind = 0;
-    full = 1;
-    activeBuff = !activeBuff;
-    
+  ind += 4; //increment the index
+  if (ind >= 512) { //if index excedes the buffer size
+    ind = 0; //reset index
+    full = 1; //set the full flag high
+    activeBuff = !activeBuff; //switch the active buffer
   }
   
-  kxAccel.getRawAccelData(&rawData);
-  digitalWrite(17, 0);
+  kxAccel.getRawAccelData(&rawData); //pole the sensor
+  digitalWrite(17, 0); //set the debugging pin low
 }
