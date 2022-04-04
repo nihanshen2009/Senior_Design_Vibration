1e64f2d33d7ea61fd46d94d3be25d0dc2fcfd8bf
nihanshen2009 <54904152+nihanshen2009@users.noreply.github.com>
2022-04-04 00:28:59 -0400

    STEM NUT DATA ANALYSIS COMPLETE

--- data_analysis.m
+++ data_analysis.m
@@ -1,20 +1,104 @@
 clear
 clc
 %% Read Byte File to Raw Data
-raw_time_file_name = "Data/time_x_y_z.txt";
+raw_time_file_name = "Data/stem nut.txt";
 fileID = fopen(raw_time_file_name, 'r')
 file_dir = dir(raw_time_file_name)
 size = file_dir.bytes % size of file in bytes
 raw_data = fread(fileID, [4, size], 'int');
-
-raw_data = raw_data((1:4),(133:size/4/4)); % trimming off zeros
+raw_data = raw_data((1:4),(33:size/4/4)); % trimming off zeros
 
 %% Convert Raw Data to G's
 conversion_factor = .0002441407513657033; % from arduino code (Yigit Testified)
 data(1,:) = raw_data(1,:);
 data(2:4,:) = raw_data(2:4,:).* conversion_factor;
-data_t = transpose(data);
 
+
+%% Hard Dividing data to different flowrate zones
+% [fft_data, f, data_time, ax, ay, az] = mai_fft(data); %entire dataset
+[fft_600, f_600, data_time_600, ax_600, ay_600, az_600] = mai_fft(data(1:4, 179275:239243)); %600 gpm data
+[fft_1100, f_1100, data_time_1100, ax_1100, ay_1100, az_1100] = mai_fft(data(1:4, 419147:479147)); %1100 gpm data
+[fft_1600, f_1600, data_time_1600, ax_1600, ay_1600, az_1600] = mai_fft(data(1:4, 599059:659069)); %1600 gpm data
+[fft_2000, f_2000, data_time_2000, ax_2000, ay_2000, az_2000] = mai_fft(data(1:4, 858963:918931)); %2000 gpm data
+
+%% Plotting 600 GPM
+figure(1)
+subplot(2,4,1);
+hold on
+plot(data_time_600, ax_600)
+plot(data_time_600, ay_600)
+plot(data_time_600, az_600)
+title('Time Series of 600 GPM')
+xlabel('time (s)') 
+ylabel('Acceleration (g)') 
+legend('acceleration_x','acceleration_y','acceleration_z')
+
+subplot(2,4,5); 
+plot(f_600,fft_600) 
+title('Frequency Domain of 600 GPM')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+ylim([0,0.01])
+
+%% Plotting 1100 GPM
+%figure(2)
+subplot(2,4,2);
+hold on
+plot(data_time_1100, ax_1100)
+plot(data_time_1100, ay_1100)
+plot(data_time_1100, az_1100)
+title('Time Series of 1100 GPM')
+xlabel('time (s)') 
+ylabel('Acceleration (g)') 
+legend('acceleration_x','acceleration_y','acceleration_z')
+
+subplot(2,4,6); 
+plot(f_1100,fft_1100) 
+title('Frequency Domain of 1100 GPM')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+ylim([0,0.05])
+
+%% Plotting 1600 GPM
+%figure(3)
+subplot(2,4,3);
+hold on
+plot(data_time_1600, ax_1600)
+plot(data_time_1600, ay_1600)
+plot(data_time_1600, az_1600)
+title('Time Series of 1600 GPM')
+xlabel('time (s)') 
+ylabel('Acceleration (g)') 
+legend('acceleration_x','acceleration_y','acceleration_z')
+
+subplot(2,4,7); 
+plot(f_1600,fft_1600) 
+title('Frequency Domain of 1600 GPM')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+ylim([0,0.01])
+
+%% Plotting 2000 GPM
+%figure(4)
+subplot(2,4,4);
+hold on
+plot(data_time_2000, ax_2000)
+plot(data_time_2000, ay_2000)
+plot(data_time_2000, az_2000)
+title('Time Series of 2000 GPM')
+xlabel('time (s)') 
+ylabel('Acceleration (g)') 
+legend('acceleration_x','acceleration_y','acceleration_z')
+
+subplot(2,4,8); 
+plot(f_2000,fft_2000) 
+title('Frequency Domain of 600 GPM')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+ylim([0,0.01])
+
+
+function [single_FFTaccel_RMS, f, time, accel_x, accel_y, accel_z] = mai_fft(data)
 time = transpose(data(1,:) ./ 1000000);
 accel_x = transpose(data(2,:));
 accel_y = transpose(data(3,:));
@@ -24,19 +108,6 @@ accel_z = transpose(data(4,:));
 accel_x = smooth(accel_x, 'loess');
 accel_y = smooth(accel_y, 'loess');
 accel_z = smooth(accel_z, 'loess');
-
-%smooth_x = lowpass(accel_x, 0.99);
-
-%{
-figure(1)
-subplot(2,1,1);
-plot(accel_x)
-title('accel_x')
-
-subplot(2,1,2);
-plot(smooth_x)
-title('smooth_x')
-%}
 
 %% FFT
 %% Mangitude of x, y, z calibration data
@@ -65,33 +136,9 @@ two_FFTaccel_RMS = abs(FFTaccel_RMS/NFFT);
 single_FFTaccel_RMS = two_FFTaccel_RMS(1:NFFT/2+1);
 single_FFTaccel_RMS(2:end-1) = 2*single_FFTaccel_RMS(2:end-1);
 
-%Compute the power of the single sided discrete Fourier transform of signal
-power_single_FFTaccel_RMS = (single_FFTaccel_RMS.^2)/NFFT;
+% %Compute the power of the single sided discrete Fourier transform of signal
+% power_single_FFTaccel_RMS = (single_FFTaccel_RMS.^2)/NFFT;
 
 
-%% Plotting time waveform and frequency spectrum of the audio signal
-figure(1)
-subplot(3,1,1);
-plot(time, accel_x)
-hold on
-plot(time, accel_y)
-plot(time, accel_z)
-title('Time waveform of signal')
-xlabel('time (s)') 
-ylabel('Amplitude') 
-legend('acceleration_x','acceleration_y','acceleration_z')
-
-subplot(3,1,2); 
-plot(f,single_FFTaccel_RMS) 
-title('Single-sided frequency spectrum of FFT signal')
-xlabel('Frequency (Hz)')
-ylabel('Magnitude')
-ylim([0,0.01])
-
-
-subplot(3,1,3); 
-plot(f,power_single_FFTaccel_RMS) 
-title('Single-sided frequency spectrum of FFT signal')
-xlabel('Frequency (Hz)')
-ylabel('Power')
+end
 
